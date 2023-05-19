915803bd67a7bc47a819bcbf1b988d6b63ec5008
Blake Ingenthron <blakeingenthron@gmail.com>
2023-05-19 15:11:58 -0500

    Update ImageList.tsx

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -9,25 +9,34 @@ const getImageFileNames = () => {
   return fileNames;
 };
 
+const getImageCount = () => {
+  const imageFileNames = getImageFileNames();
+  return imageFileNames.length;
+};
+
 const ImageList = () => {
   const imageFileNames = getImageFileNames();
+  const imageCount = getImageCount();
 
   return (
-    <ul className="grid my-32 w-full grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-      {imageFileNames.map((fileName) => (
-        <li className="relative" key={fileName}>
-          <div className="aspect-square block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-            <Image
-              className="pointer-events-none object-contain group-hover:opacity-75"
-              fill
-              src={`/images/${fileName}`}
-              alt={fileName}
-              sizes="(max-width: 640px) 100vw, (max-width: 1024px) 33vw, 25vw"
-            />
-          </div>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h2 className="mt-32 mb-16 text-xl">Number of images: {imageCount}</h2>
+      <ul className="grid mb-32 w-full grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+        {imageFileNames.map((fileName) => (
+          <li className="relative" key={fileName}>
+            <div className="aspect-square block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+              <Image
+                className="pointer-events-none object-contain group-hover:opacity-75"
+                fill
+                src={`/images/${fileName}`}
+                alt={fileName}
+                sizes="(max-width: 640px) 100vw, (max-width: 1024px) 33vw, 25vw"
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
